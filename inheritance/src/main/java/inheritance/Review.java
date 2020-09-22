83cbe15ba088245c120c492203abbfbd03188dc6
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-09-21 19:59:23 -0700

    small changes

--- inheritance/src/main/java/inheritance/Review.java
+++ inheritance/src/main/java/inheritance/Review.java
@@ -7,24 +7,18 @@ public class Review {
     private String body;
     private String author;
     private int stars1;
-//    private Restaurant restaurant;
     public static ArrayList<Restaurant> allRestaurants= new ArrayList<>();
-    // create an array of reviews for each restaurant as an hashmap
     private static ArrayList<Review> reviews = new ArrayList<>();
     public static HashMap<Restaurant, ArrayList<Review>> restaurantAndReviews = new HashMap<>();
 
 
     public Review(String body, String author, int stars1) {
-//        this.restaurant = restaurant;
         this.body = body;
         this.author = author;
         this.stars1 = stars1;
-//        this.addToList();
-//        restaurant.addReview(this);
     }
 
     public String toString(){
-//        return String.format("%d. Restaurant: %s, Review: %s, Author: %s,  Stars: %d", reviewCount, restaurant, body, author, stars1);
         return String.format("Review: %s, Author: %s,  Stars: %d", body, author, stars1);
 
     }
